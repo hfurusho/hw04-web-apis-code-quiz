09efde7e9fef1bfc99a0ad0e0f461721b820031f
Kami <51175640+hfurusho@users.noreply.github.com>
2019-10-09 13:14:06 -0700

    Basic landing format and styling.

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,6 +1,16 @@
 let questions = [
   {
-    question: "Sample Question",
+    question: "Sample Question 1",
+    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+    answer: "Choice 1"
+  },
+  {
+    question: "Sample Question 2",
+    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+    answer: "Choice 2"
+  },
+  {
+    question: "Sample Question 3",
     choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
     answer: "Choice 3"
   }
