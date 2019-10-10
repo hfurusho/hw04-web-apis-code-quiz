0c190a644af68aa2ad07b84c89e9d348a86309bf
Kami <51175640+hfurusho@users.noreply.github.com>
2019-10-09 19:54:07 -0700

    Added start button functionality. Sets the time, shows the questions card, and begins countdown.

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,22 @@
-let timerInterval = setInterval(timeCountdown, 1000)
+$("#start-btn").on("click", startQuiz);
 
-function timeCountdown() {
-  let timeRemEl = document.getElementById("timeRem");
-  let timeCurr = timeRemEl.textContent;
-  if (timeCurr === "0") {
-    timeRemEl.textContent = "Times Up!"
-    // TODO: FUNCTION TO STOP THE GAME
-    clearInterval(timerInterval);
-  } else {
-    timeRemEl.textContent = parseInt(timeCurr, 10) - 1;
+function startQuiz() {
+  $("#start-card").addClass("d-none");
+  // TODO: SHOW QUESTIONS CARD. Add "d-none" to .questions-card
+  $("#questions-card").addClass("d-block");
+  let timerInterval = setInterval(timeCountdown, 1000);
+  let totalTime = questions.length * 15;
+  let timeRemEl = $("#timeRem");
+  timeRemEl.text(totalTime);
+
+  function timeCountdown() {
+    let timeCurr = timeRemEl.text();
+    if (timeCurr === "0") {
+      timeRemEl.text("Times Up!");
+      // TODO: FUNCTION TO STOP THE GAME
+      clearInterval(timerInterval);
+    } else {
+      timeRemEl.text(parseInt(timeCurr, 10) - 1);
+    }
   }
 }
