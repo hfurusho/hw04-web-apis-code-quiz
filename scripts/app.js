3b33e229a45cf5422f45a0deeac2126f6460b055
Kami <51175640+hfurusho@users.noreply.github.com>
2019-10-10 13:18:52 -0700

    Code refactor. Question card will be populated with first question on quiz start.

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,64 @@
 $("#start-btn").on("click", startQuiz);
 
+// Starts the quiz by hiding the start-card, showing the questions card, and beginning the timer.
 function startQuiz() {
-  $("#start-card").addClass("d-none");
+  // $("#start-card").addClass("d-none");
   // TODO: SHOW QUESTIONS CARD. Add "d-none" to .questions-card
-  $("#questions-card").addClass("d-block");
-  let timerInterval = setInterval(timeCountdown, 1000);
-  let totalTime = questions.length * 15;
-  let timeRemEl = $("#timeRem");
-  timeRemEl.text(totalTime);
+  // $("#questions-card").addClass("d-block");
 
-  function timeCountdown() {
-    let timeCurr = timeRemEl.text();
-    if (timeCurr === "0") {
-      timeRemEl.text("Times Up!");
-      // TODO: FUNCTION TO STOP THE GAME
-      clearInterval(timerInterval);
-    } else {
-      timeRemEl.text(parseInt(timeCurr, 10) - 1);
+  TODOname(1);
+  startTimer();
+
+  function TODOname(questionNum) {
+    let question = questions[questionNum - 1];
+    $("#question").text(question.question);
+
+    let choiceButtons = $(".choice-btn");
+    choiceButtons.each(function(i) {
+      $(this).text(question.choices[i]);
+    });
+
+    // for (let i = 0; i < choiceButtons.length; i++) {
+    //   choiceButtons[i].text(question.choices[i]);
+    // }
+  }
+
+  function startTimer() {
+    let timerInterval = setInterval(timeCountdown, 1000);
+    let totalTime = questions.length * 15;
+    $("#timeRem").text(totalTime);
+
+    // Reduces the time remaining and ends the game when the timer reaches 0;
+    // TODO: stop game if all questions answered.
+    function timeCountdown() {
+      let timeCurr = $("#timeRem").text();
+      if (timeCurr === "0") {
+        $("#timeRem").text("Times Up!");
+        // TODO: FUNCTION TO STOP THE GAME
+        clearInterval(timerInterval);
+      } else {
+        $("#timeRem").text(parseInt(timeCurr, 10) - 1);
+      }
     }
   }
+}
+
+// Takes an array and outputs the array with the contents shuffled.
+// Taken from https://bost.ocks.org/mike/shuffle/
+function shuffle(array) {
+  let m = array.length;
+  let t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
 }
--- scripts/questions.js
+++ scripts/questions.js
@@ -1,17 +1,17 @@
 let questions = [
   {
     question: "Sample Question 1",
-    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-    answer: "Choice 1"
+    choices: ["Choice 1-1", "Choice 1-2", "Choice 1-3", "Choice 1-4"],
+    answer: "Choice 1-1"
   },
   {
     question: "Sample Question 2",
-    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-    answer: "Choice 2"
+    choices: ["Choice 2-1", "Choice 2-2", "Choice 2-3", "Choice 2-4"],
+    answer: "Choice 2-2"
   },
   {
     question: "Sample Question 3",
-    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-    answer: "Choice 3"
+    choices: ["Choice 3-1", "Choice 3-2", "Choice 3-3", "Choice 3-4"],
+    answer: "Choice 3-3"
   }
 ];
