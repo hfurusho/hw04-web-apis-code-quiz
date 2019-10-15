887e8357b7964dccc4c16dbaa201d93cbabb0c1a
Kami <51175640+hfurusho@users.noreply.github.com>
2019-10-14 18:02:59 -0700

    Added quiz topic, questions, answers, and instructions

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,17 +1,57 @@
 let questions = [
   {
-    question: "Sample Question 1",
-    choices: ["Choice 1-1", "Choice 1-2", "Choice 1-3", "Choice 1-4"],
-    answer: "Choice 1-1"
+    question: "Cuttlefish are:",
+    choices: ["a fish", "a Molluscs", "a Cnidaria", "an Arthropod"],
+    answer: "a Molluscs"
   },
   {
-    question: "Sample Question 2",
-    choices: ["Choice 2-1", "Choice 2-2", "Choice 2-3", "Choice 2-4"],
-    answer: "Choice 2-2"
+    question: "How many species of cuttlefish are there?",
+    choices: ["1", "About 60", "Over 120", "Over 9000"],
+    answer: "Over 120"
   },
   {
-    question: "Sample Question 3",
-    choices: ["Choice 3-1", "Choice 3-2", "Choice 3-3", "Choice 3-4"],
-    answer: "Choice 3-3"
+    question: "The flamboyant cuttlefish is known because it is:",
+    choices: [
+      "the most fabulous",
+      "one of the smallest of its species.",
+      "the only poisonous cuttlefish.",
+      "all of these."
+    ],
+    answer: "all of these."
+  },
+  {
+    question: "How many hearts do cuttlefish have?",
+    choices: ["One", "Three", "None", "As many as the cuttlefish can capture."],
+    answer: "Three"
+  },
+  {
+    question: "For defense, cuttlefish have",
+    choices: [
+      "camouflage",
+      "ink",
+      "a jet-propulsion mechanism",
+      "all of these skills."
+    ],
+    answer: "all of these skills."
+  },
+  {
+    question: "Cuttlefish have a shell called:",
+    choices: [
+      "the cuttlebone",
+      "the pod",
+      "they do not have shell.",
+      "shellton"
+    ],
+    answer: "the cuttlebone"
+  },
+  {
+    question: "A Cuttlefish's brain is",
+    choices: [
+      "bigger than its body",
+      "the samllest in the world.",
+      "completely split in half",
+      "None of these."
+    ],
+    answer: "bigger than its body"
   }
 ];
